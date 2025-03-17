1d8a5b9b949228b2f909c9b5dd5f30c3f149533c
Joao Genaro <JoaoGenaro@Joaos-Macbook-Pro.local>
2025-03-17 14:44:31 -0300

    Bits/stdc++.h

---
+++ bits/stdc++.h/bits/stdc++.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <cmath>
+#include <algorithm>
+#include <limits>
+#include <string>
+#include <bitset>
+#include <cstring>
+#include <cassert>
+using namespace std;
